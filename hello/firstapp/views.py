6da029e59696af030efb4114c994985900e129bb
Alexander Mangazeev <alex3287@bk.ru>
2022-03-29 20:07:03 +0300

    add data

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -3,8 +3,13 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'firstapp/home.html')
-
+    # return render(request, 'firstapp/home.html')
+    header = "Персональные данные"
+    langs = ["Английский", "Испанский", "Русский",]
+    user = {"name": "Максим,", "age": 30}
+    addr = ("Виноградная", 23, 45)
+    data = {"header": header, "langs": langs, "user": user, "address": addr}
+    return render(request, "index.html", context=data)
 
 def about(request):
     return HttpResponse("<H2>О сайте</H2>")
