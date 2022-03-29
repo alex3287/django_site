c74772b92f389b58e1f7da269cbe81a7c88733cf
Alexander Mangazeev <alex3287@bk.ru>
2022-03-29 19:48:19 +0300

    new change

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,13 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'firstapp/home.html')
+
 
 def about(request):
     return HttpResponse("<H2>О сайте</H2>")
+
 
 def contact(request):
     return HttpResponse("<H2>Контакты</H2>")
